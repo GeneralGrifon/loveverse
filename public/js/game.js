5bde6a56dd3d868a27208b65b7409015aa072946
GeneralGrifon <56627540+GeneralGrifon@users.noreply.github.com>
2022-11-23 17:41:23 +0400

    Add files via upload

---
+++ public/js/game.js
@@ -0,0 +1,110 @@
+// var fbRef = new firebase ("https://devchata-1410d-default-rtdb.firebaseio.com");
+// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+// import { Player } from "./player"
+var otherPlayers = {};
+
+var playerID;
+var player;
+
+function loadGame() {
+	// load the environment
+	// loadEnvironment();
+	// load the player
+	initMainPlayer();
+
+	listenToOtherPlayers();
+
+	loadWorld(); 
+
+	window.onunload = function() {
+		fbRef.child( "Playerzz/" + playerID ).remove();
+	};
+
+	window.onbeforeunload = function() {
+		fbRef.child( "Playerzz/" + playerID ).remove();
+	};
+}
+
+function listenToPlayer( playerData ) {
+	if ( playerData.val() ) {
+		otherPlayers[playerData.key].setOrientation( playerData.val().orientation.position, playerData.val().orientation.rotation );
+	}
+}
+
+function listenToOtherPlayers() {
+	// when a player is added, do something
+	fbRef.child( "Playerzz" ).on( "child_added", function( playerData ) {
+		if ( playerData.val() ) {
+			if ( playerID != playerData.key && !otherPlayers[playerData.key] ) {
+				otherPlayers[playerData.key] = new Player( playerData.key );
+				otherPlayers[playerData.key].init();
+				fbRef.child( "Playerzz/" + playerData.key ).on( "value", listenToPlayer );
+			}
+		}
+	});
+
+	// when a player is removed, do something
+
+	fbRef.child( "Playerzz" ).on( "child_removed", function( playerData ) {
+		if ( playerData.val() ) {
+			fbRef.child( "Playerzz/" + playerData.key ).off( "value", listenToPlayer );
+			scene.remove( otherPlayers[playerData.key].mesh );
+			delete otherPlayers[playerData.key];
+		}
+	});
+}
+
+function initMainPlayer() {
+
+	fbRef.child( "Playerzz/" + playerID ).set({
+		isOnline: true,
+		orientation: {
+			position: {x: 0, y:0, z:0},
+			rotation: {x: 0, y:0, z:0}
+		}
+	});
+
+	
+
+	player = new Player( playerID );
+	player.isMainPlayer = true;
+	player.init();
+}
+
+function loadEnvironment() {
+
+
+
+}
+
+function loadWorld() {
+	  
+	  {
+		const planeSize = 10;
+	
+		var loader = new THREE.TextureLoader();
+		var texture = loader.load('/public/js/textures/grass.jpg');
+		texture.wrapS = THREE.RepeatWrapping;
+		texture.wrapT = THREE.RepeatWrapping;
+		texture.magFilter = THREE.NearestFilter;
+		var repeats = planeSize / 1;
+		texture.repeat.set(repeats, repeats);
+	
+		var planeGeo = new THREE.PlaneBufferGeometry(planeSize, planeSize);
+		var planeMat = new THREE.MeshPhongMaterial({
+		  map: texture,
+		  side: THREE.DoubleSide,
+		});
+		var mesh = new THREE.Mesh(planeGeo, planeMat);
+		mesh.rotation.x = Math.PI * -.5;
+		scene.add(mesh);
+	  }
+	  
+}
+
+
+
+
+	
+
+	
---
+++ public/js/main.js
@@ -0,0 +1,95 @@
+
+var container, scene, camera, renderer;
+
+var controls;
+
+init();
+animate();
+
+function init() {
+	// Setup
+	container = document.getElementById( 'container' );
+
+	scene = new THREE.Scene();
+	{
+		var skyColor = 0xB1E1FF;  // light blue
+		var groundColor = 0xB97A20;  // brownish orange
+		var intensity = 1;
+		var light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+		scene.add(light);
+	  }
+	
+	  {
+		var color = 0xFFFFFF;
+		var intensity = 1;
+		var light = new THREE.DirectionalLight(color, intensity);
+		light.position.set(5, 10, 2);
+		scene.add(light);
+		scene.add(light.target);
+	  }
+
+
+	camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 1000 );
+	camera.position.z = 5;
+
+	renderer = new THREE.WebGLRenderer( { alpha: false} );
+	renderer.setSize( window.innerWidth, window.innerHeight);
+
+	// Load game world
+
+	firebase.auth().onAuthStateChanged(function( user ) {
+		if ( user ) {
+			// User is signed in
+
+			console.log( "Player is signed in " );
+			playerID = user.uid;
+
+			fbRef.child( "Players/" + playerID + "/isOnline" ).once( "value" ).then( function( isOnline ) {
+
+				if ( isOnline.val() === null || isOnline.val() === false ) {
+					loadGame();
+				} else {
+					alert( "Hey, only one session at a time buddy!" );
+				}
+			});
+
+
+		} else {
+			// User is signed out
+			console.log( "Player is signed out " );
+
+			firebase.auth().signInAnonymously().catch(function(error) {
+				console.log( error.code + ": " + error.message );
+			})
+		}
+	});
+
+	// Events
+	window.addEventListener( "resize", onWindowResize, false );
+
+	container.appendChild( renderer.domElement );
+	document.body.appendChild( container );
+}
+
+function animate() {
+	requestAnimationFrame( animate );
+
+	if ( controls ) {
+		controls.update();
+	}
+	
+	render();
+}
+
+function render() {
+
+	renderer.clear();
+	renderer.render( scene, camera );
+}
+
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	
+	renderer.setSize( window.innerWidth, window.innerHeight );
+}
